4fe1550b594b1d59042af4ec32d24450102cdceb
Imana Ohran <imana.ohran.20@size.ba>
2025-01-22 15:36:23 +0100

    Added model Task

---
+++ src/main/java/com/app/todo/models/Task.java
@@ -0,0 +1,16 @@
+package com.app.todo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Task {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private boolean completed;
+}
